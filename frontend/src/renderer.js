cb5b036ab92bb09909bfd405b838ba2455d46fb0
Abdul Rehman <abdulr568programming@outlook.com>
2025-06-09 18:03:51 +0100

    config: Installed Tailwind CSS

--- frontend/src/renderer.js
+++ frontend/src/renderer.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const App = () => { return <h1>Hello from React + Electron!</h1>; };
+const App = () => { return <h1 className = "text-3xl font-bold underline text-blue-600 p-4">Hello from React + Electron with Tailwind!</h1>; };
 
 const root = createRoot(document.getElementById("root"));
 root.render(<App />);
